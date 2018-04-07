26bf756ee2803f05cfa88a4ac24c44a0dab94535
hirenpatel <hiren.patel@mademedia.co.uk>
2018-04-07 17:37:45 +0100

    Surrounding text in span for better styling

--- src/templates/block-control.js
+++ src/templates/block-control.js
@@ -8,7 +8,7 @@ module.exports = (block) => {
       <svg role="img" class="st-icon">
         <use xlink:href="${config.defaults.iconUrl}#${block.icon_name}"/>
       </svg>
-      ${block.title()}
+      <span class="st-label">${block.title()}</span>
     </button>
   `;
 };
